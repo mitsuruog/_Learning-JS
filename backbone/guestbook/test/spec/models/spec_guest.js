191d3833b8de00a49f401d3af7ce84f20bf55a1a
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2012-11-16 02:50:12 -0500

    add unittest for guestbook's models

---
+++ backbone/guestbook/test/spec/models/spec_guest.js
@@ -0,0 +1,24 @@
+describe('guest Model spec', function() {
+    beforeEach(function() {
+        this.guest = new GUEST.Model.guest();
+    });
+    
+    it('create model with default value', function() {
+        expect(this.guest.get('name')).toEqual(null);
+    });
+    
+    it('set name to model', function() {
+        this.guest.set({name: 'mitsuruog'});
+        expect(this.guest.get('name')).toEqual('mitsuruog');
+    });
+    
+    it('fire event when the model name changed', function() {
+        //spy
+        var spy = jasmine.createSpy('-change event callback-');
+        this.guest.on('change', spy);
+        
+        //do test
+        this.guest.set({name: 'mitsuruog'});
+        expect(spy).toHaveBeenCalled();
+    });    
+});
