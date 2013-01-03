29930df3627dcc4f2015d6b5fb6366f30a2e05c8
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2013-01-03 09:43:00 -0500

    fix some

--- core/scope_chain/scope_chain.js
+++ core/scope_chain/scope_chain.js
@@ -3,15 +3,15 @@
  */
 var hoge = 'hoge', fuga = 'fuga';
 
-console.log('g[hoge]->' + hoge);
+console.log('global[hoge]->' + hoge);
 
 (function(){
     var hoge = 'hogehoge';
-    console.log('l[hoge]->' + hoge);
+    console.log('local[hoge]->' + hoge);
     //スコープチェーンでグローバル参照
-    console.log('l[fuga]->' + fuga);
+    console.log('local[fuga]->' + fuga);
 })();
-console.log('g[hoge]->' + hoge);
+console.log('global[hoge]->' + hoge);
 
 
 /**
@@ -20,34 +20,38 @@ console.log('g[hoge]->' + hoge);
 var obj = {};
 obj.hoge = 'obj.hoge';
 
-console.log('g[obj.hoge]->' + obj.hoge);
+console.log('global[obj.hoge]->' + obj.hoge);
 (function(){
     //参照元プロパティの内容を変更
     obj.hoge = 'hogehoge';
-    console.log('l[obj.hoge]->' + obj.hoge);
+    console.log('local[obj.hoge]->' + obj.hoge);
 })();
 //置き換わっている
-console.log('g[obj.hoge]->' + obj.hoge);
+console.log('global[obj.hoge]->' + obj.hoge);
 
 //削除->undefinedの確認
 delete obj.hoge;
-console.log('g[obj.hoge]->' + obj.hoge);
+console.log('global[obj.hoge]->' + obj.hoge);
 
 
 /**
  * 参照型でwith文を使用したスコープチェーンの確認
  */
 obj.hoge = 'obj.hoge';
-console.log('g[obj.hoge]->' + obj.hoge);
+console.log('global[obj.hoge]->' + obj.hoge);
 
 with(obj){
-    console.log('w[obj.hoge]->' + hoge);
+    console.log('with[obj.hoge]->' + hoge);
+
+    //これはobj.fugaではなくグローバルfugaを参照...か
+    console.log('with[obj.fuga]->' + fuga);
+
     //参照元プロパティの内容を変更
     delete obj.hoge;
     //ななななんと！スコープチェーンを遡ってhogeを参照してしまう...orz
-    console.log('w[obj.hoge]->' + hoge);
+    console.log('with[obj.hoge]->' + hoge);
 }
 //ここではundefined
-console.log('g[obj.hoge]->' + obj.hoge);
+console.log('global[obj.hoge]->' + obj.hoge);
 
 
